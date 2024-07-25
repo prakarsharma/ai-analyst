5297535849f0c28b87c588790ee3b1bd331d1002
Prakarsh <prakarsh.sharma@walmart.com>
2024-07-25 14:19:29 +0000

    added platform configs

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,12 @@
 import yaml
+from typing import Dict
 
+
+def concat(loader, node):
+    seq = loader.construct_sequence(node)
+    return ''.join([str(_) for _ in seq])
+
+yaml.SafeLoader.add_constructor(tag='!concat', constructor=concat) # register the tag handler
 
 with open("resources/config.yml", "r") as f:
-    conf = yaml.safe_load(f)
+    conf:Dict = yaml.safe_load(f)
