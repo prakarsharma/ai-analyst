dd85946d84c7cdceb220ae0e041e6498bffcdc93
Prakarsh <prakarsh.sharma@walmart.com>
2024-08-09 09:50:46 +0000

    implemented high probability match retrieval with a second pass

--- app/functions.py
+++ app/functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 from typing import List, Dict
 from functools import lru_cache
 
-from app.knowledge import vector_db
+from app.knowledge import vectorDB
 from utils.config import conf
 from utils.utils import (create_vertexai_bigquery_client, 
                          create_element_bigquery_connection)
@@ -33,20 +33,44 @@ class bigquery_job:
 
 
 bq = bigquery_job()
-metrics_db = vector_db("metrics")
+metrics_db = vectorDB("metrics")
 
 def get_mapping(query:str, **kwargs) -> List[Dict[str,str]]:
     results = {"mapping": bq.run(query)}
     for record in results["mapping"]:
         if "sbu" in record:
             results["sbu"] = record["sbu"]
-            results["table_id"] = conf["bigquery"][results["sbu"].lower()]['table_id']
-            results["schema"] = conf["bigquery"][results["sbu"].lower()]["schema"]
+            metadata = conf["bigquery"][results["sbu"].lower()]
+            results.update(metadata)
             if not results["schema"]:
                 results["schema"] = get_table_schema(results["table_id"])
-            results["relevant_metrics"] = metrics_db.query(kwargs.get("user_prompt", ""))
+            relevant_metrics = find_relevant_metrics(kwargs.get("user_prompt"))
+            results.update(relevant_metrics)
             break
     return results
+
+def find_relevant_metrics(prompt:str, 
+                          metrics:pd.Series=conf["knowledge"]["documents"]["metrics"]["metric"], 
+                          definitions:pd.Series=conf["knowledge"]["documents"]["metrics"]["definition"], 
+                          limit:int=3, 
+                          **kwargs) -> Dict[str, List[str]]:
+    search_result = metrics_db.query(prompt)
+    matches = metrics_db.match(search_result, limit)
+    relevant_metrics = [f"{metrics.get(int(_id))}: {definitions.get(int(_id))}" for _id in matches]
+    supporting_document_matches = find_supporting_documents([definitions.get(int(_id)) for _id in matches])
+    supporting_documents = [f"{metrics.get(int(_id))}: {definitions.get(int(_id))}" for _id in supporting_document_matches if _id not in matches]
+    return {
+        "relevant_metrics": relevant_metrics, 
+        "supporting_documents": supporting_documents
+    }
+
+def find_supporting_documents(definitions:List[str], limit:int=3) -> List[str]:
+    supporting_documents = []
+    for _ in definitions:
+        search_result = metrics_db.query(_)
+        matches = metrics_db.match(search_result, limit)
+        supporting_documents.extend(matches)
+    return list(set(supporting_documents))
 
 @lru_cache
 def get_mapping_table(table_id:str=conf["bigquery"]["mapping"]['table_id'], **kwargs) -> Dict:
--- app/knowledge.py
+++ app/knowledge.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from scipy.special import softmax
+from sklearn.metrics.pairwise import cosine_similarity
 from requests import (request, 
                       models)
 from chromadb import (EmbeddingFunction, 
@@ -31,23 +34,49 @@ class embeddingModel(EmbeddingFunction):
             return embedding_request.parse_response(response)
 
 
-def get_or_create_vector_db(name:str, documents:List[str]) -> Collection:
-    client = Client()
-    db:Collection = client.get_or_create_collection(name=name, embedding_function=embeddingModel(name))
-    for i, doc in enumerate(documents):
-        db.upsert(documents=doc, ids=str(i))
-    return db
+class vectorDB:
+    def __init__(self, 
+                 name:str, 
+                 documents:pd.Series=conf["knowledge"]["documents"]["metrics"]["metric"], 
+                 distance:str="cosine"):
+        client = Client()
+        self.db:Collection = client.get_or_create_collection(name=name, 
+                                                             embedding_function=embeddingModel(), 
+                                                             metadata={"hnsw:space": distance})
+        self.upsert([*documents], name)
 
+    @property
+    def n_docs(self) -> int:
+        return self.db.count()
 
-class vector_db:
-    def __init__(self, name:str):
-        documents:List[str] = conf["vector_db"]["documents"][name].split("\n\n")
-        self.db = get_or_create_vector_db(name, documents)
+    def upsert(self, documents:List[str], name:str):
+        _id = self.n_docs
+        for i, doc in enumerate(documents):
+            self.db.upsert(documents=[doc], ids=[str(_id+i)], metadatas=[{"repository": name}])
 
-    def query(self, query_texts:str="", n_results:int=1):
+    def query(self, query_text:str, top_n:int=0, **metadata) -> Dict[str, List[List]]:
+        if query_text:
+            kwargs = {
+                "query_texts": query_text, 
+                "n_results": top_n or self.n_docs, 
+                "include": ["embeddings", "distances", "documents"]
+            }
+            if metadata:
+                kwargs.update({"where": metadata})
+            result = self.db.query(**kwargs)
+            return result
+        return query_texts
+
+    def match(self, query_result:Dict[str,List[List]], limit:int=1) -> List[str]:
         try:
-            if query_texts:
-                return self.db.query(query_texts=query_texts, n_results=n_results)["documents"][0][0]
-            return query_texts
-        except (KeyError, IndexError) as err:
-            raise ValueError("!corrupt vector search response!")
+            similarities = 1 - pd.Series(query_result["distances"][0], index=query_result["ids"][0])
+            # similarities.loc[-1] = 0 # a result equivalent to random noise
+            probabilities = pd.Series(softmax(similarities.values), index=query_result["ids"][0])
+            matches = probabilities.loc[probabilities > self.p_uniform]
+            return matches.index.tolist()[:limit]
+        except IndexError as err:
+            raise ValueError("!bad vector search response!")
+
+    @property
+    def p_uniform(self):
+        return 1/ self.n_docs
