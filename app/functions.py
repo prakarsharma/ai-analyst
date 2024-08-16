83c3b952f2e8feef651a18bf61cc37f3fa0af6cb
Prakarsh <prakarsh.sharma@walmart.com>
2024-08-16 08:28:56 +0000

    added a new function

--- app/functions.py
+++ app/functions.py
@@ -7,7 +7,7 @@ from utils.config import conf
 from utils.utils import bigquery_job
 
 
-def get_mapping(query:str, **kwargs) -> List[Dict[str,str]]:
+def get_mapping(table_id:str, query:str, **kwargs) -> List[Dict[str,str]]:
     """
     Fetch data from mapping table.
 
@@ -16,16 +16,11 @@ def get_mapping(query:str, **kwargs) -> List[Dict[str,str]]:
     dict
         Records returned from a mapping table query.
     """
-    results = {"mapping": bigquery_job.run(query)}
-    for record in results["mapping"]:
-        if "sbu" in record:
-            results["sbu"] = record["sbu"]
-            metadata = conf["bigquery"][results["sbu"].lower()]
-            results.update(metadata)
-            relevant_metrics = find_relevant_metrics(kwargs.get("user_prompt"))
-            results.update(relevant_metrics)
-            break
-    return results
+    if table_id != conf["bigquery"]["mapping"]["table_id"]:
+        return {
+            "error": f"'{table_id}' is not the correct table ID for the mapping table. Call 'get_mapping_table' to get the correct table ID."
+        }
+    return bigquery_job.run(query)
 
 @lru_cache
 def get_mapping_table(**kwargs) -> Dict:
@@ -40,7 +35,7 @@ def get_mapping_table(**kwargs) -> Dict:
     metadata = conf["bigquery"]["mapping"]
     return metadata
 
-def fetch_data(sbu:str, query:str, **kwargs) -> List[Dict[str,str]]:
+def fetch_data(sbu:str, table_id:str, query:str, **kwargs) -> List[Dict]:
     """
     Fetch data from BigQuery table and/ or find or compute the different relevant metrics. Data for only one SBU can be considered at one time.
     
@@ -49,4 +44,31 @@ def fetch_data(sbu:str, query:str, **kwargs) -> List[Dict[str,str]]:
     dict
         Records returned from SBU data table query.
     """
+    metadata = conf["bigquery"].get(sbu.lower())
+    if not metadata:
+        return {
+            "error": f"'{sbu}' is not a valid SBU name. Get valid SBU name from mapping table."
+        }
+    if table_id != metadata["table_id"]:
+        return {
+            "error": f"'{table_id}' is not the correct table ID for '{sbu}' data table. Call 'get_sbu_table' to get the correct table ID."
+        }
     return bigquery_job.run(query)
+
+@lru_cache
+def get_sbu_table(sbu:str, **kwargs) -> Dict:
+    """
+    Get SBU data table ID and related metadata.
+    
+    Returns
+    -------
+    dict
+        Table ID, table data description, table schema and primary keys. Along with metadata a set of relevant metrics (w.r.t. the user's question), their definitions and some supporting information is also returned.
+    """
+    metadata = conf["bigquery"].get(sbu.lower())
+    if not metadata:
+        return {
+            "error": f"'{sbu}' is not a valid SBU name. Get valid SBU name from mapping table."
+        }
+    metadata.update(find_relevant_metrics(kwargs.get("user_prompt")))
+    return metadata
--- models_api/function_template.py
+++ models_api/function_template.py
@@ -1,16 +1,23 @@
 from app import functions
-tools = [{
+
+tools = [
+    {
         "name": functions.get_mapping.__name__,
         "description": functions.get_mapping.__doc__,
         "parameters": {
             "type": "object",
             "properties": {
+                "table_id": {
+                    "type": "string", 
+                    "description": "table ID of the mapping table"
+                },
                 "query": {
                     "type": "string", 
                     "description": "a SQL query to fetch details from the mapping table. If the query does not contain all the primary key columns make sure it deduplicates records."
                 }
             },
             "required": [
+                "table_id",
                 "query"
             ],
         }
@@ -36,6 +43,10 @@ tools = [{
                     "type": "string",
                     "description": "the SBU for which data has to be fetched"
                 },
+                "table_id": {
+                    "type": "string", 
+                    "description": "table ID of the SBU data table"
+                },
                 "query": {
                     "type": "string",
                     "description": "a SQL query. Note that the query should be in Google SQL syntax. If the query does not contain all the primary key columns make sure it deduplicates records. The result should always be aggregated, with or without grouping by, unless the user's question requires only to list the records."
@@ -43,7 +54,25 @@ tools = [{
             },
             "required": [
                 "sbu",
+                "table_id",
                 "query"
+            ],
+        }
+    },
+
+    {
+        "name": functions.get_sbu_table.__name__, 
+        "description": functions.get_sbu_table.__doc__, 
+        "parameters": {
+            "type": "object", 
+            "properties": {
+                "sbu": {
+                    "type": "string", 
+                    "description": "name of SBU for which data has to be fetched"
+                }
+            }, 
+            "required": [
+                "sbu"
             ],
         }
     }
