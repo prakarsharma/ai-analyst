d59bd26e61b3fdffb86cfc25930284d348114f48
Prakarsh <prakarsh.sharma@walmart.com>
2024-08-19 12:21:44 +0000

    updated function calling prompts and knowledge base

--- app/functions.py
+++ app/functions.py
@@ -9,7 +9,7 @@ from utils.utils import bigquery_job
 
 def get_mapping(table_id:str, query:str, **kwargs) -> List[Dict[str,str]]:
     """
-    Fetch data from mapping table.
+    Fetch plan ID, department and SBU details from mapping table.
 
     Returns
     -------
@@ -30,7 +30,7 @@ def get_mapping_table(**kwargs) -> Dict:
     Returns
     -------
     dict
-        Table ID, table data description, table schema and primary keys.
+        Mapping table ID, data description, table schema and primary keys.
     """
     metadata = conf["bigquery"]["mapping"]
     return metadata
@@ -47,11 +47,11 @@ def fetch_data(sbu:str, table_id:str, query:str, **kwargs) -> List[Dict]:
     metadata = conf["bigquery"].get(sbu.lower())
     if not metadata:
         return {
-            "error": f"'{sbu}' is not a valid SBU name. Get valid SBU name from mapping table."
+            "error": f"'{sbu}' is not a valid SBU name. Call 'get_mapping' to get a valid SBU name from mapping table."
         }
     if table_id != metadata["table_id"]:
         return {
-            "error": f"'{table_id}' is not the correct table ID for '{sbu}' data table. Call 'get_sbu_table' to get the correct table ID."
+            "error": f"'{table_id}' is not the correct table ID for SBU '{sbu}'. Call 'get_sbu_table' to get the correct table ID for the SBU."
         }
     return bigquery_job.run(query)
 
@@ -63,12 +63,12 @@ def get_sbu_table(sbu:str, **kwargs) -> Dict:
     Returns
     -------
     dict
-        Table ID, table data description, table schema and primary keys. Along with metadata a set of relevant metrics (w.r.t. the user's question), their definitions and some supporting information is also returned.
+        SBU data table ID, data description, table schema and primary keys. Along with metadata a set of relevant metrics (relevant to the user's question), their definitions and some supporting information is also returned.
     """
     metadata = conf["bigquery"].get(sbu.lower())
     if not metadata:
         return {
-            "error": f"'{sbu}' is not a valid SBU name. Get valid SBU name from mapping table."
+            "error": f"'{sbu}' is not a valid SBU name. Call 'get_mapping' function to get a valid SBU name from mapping table."
         }
     metadata.update(find_relevant_metrics(kwargs.get("user_prompt")))
     return metadata
--- models_api/function_template.py
+++ models_api/function_template.py
@@ -9,7 +9,7 @@ tools = [
             "properties": {
                 "table_id": {
                     "type": "string", 
-                    "description": "table ID of the mapping table"
+                    "description": "table ID of the mapping table. You can get it using 'get_mapping_table' function."
                 },
                 "query": {
                     "type": "string", 
@@ -41,15 +41,15 @@ tools = [
             "properties": {
                 "sbu": {
                     "type": "string",
-                    "description": "the SBU for which data has to be fetched"
+                    "description": "name of the SBU for which data has to be fetched. You can get it from the mapping table using 'get_mapping' function."
                 },
                 "table_id": {
                     "type": "string", 
-                    "description": "table ID of the SBU data table"
+                    "description": "table ID of the SBU data table. You can get it using 'get_sbu_table' function."
                 },
                 "query": {
                     "type": "string",
-                    "description": "a SQL query. Note that the query should be in Google SQL syntax. If the query does not contain all the primary key columns make sure it deduplicates records. The result should always be aggregated, with or without grouping by, unless the user's question requires only to list the records."
+                    "description": """a SQL query. Note that the query should be in Google SQL syntax. If the query does not contain all the primary key columns make sure it deduplicates records. The result should always be aggregated and may or may not be grouped by appropriate column(s). Do not group by numeric columns. Avoid zero-division error in calculation. Sort results by suitable columns and use appropriate aliases where applicable."""
                 }
             },
             "required": [
@@ -68,7 +68,7 @@ tools = [
             "properties": {
                 "sbu": {
                     "type": "string", 
-                    "description": "name of SBU for which data has to be fetched"
+                    "description": "name of the SBU for which data has to be fetched. You can get it from the mapping table using 'get_mapping' function."
                 }
             }, 
             "required": [
