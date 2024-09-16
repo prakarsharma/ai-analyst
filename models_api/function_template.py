081dce94e606220bcaffd1ac5d1390553d601132
Prakarsh <prakarsh.sharma@walmart.com>
2024-09-16 13:08:49 +0000

    updated system and function prompts

--- models_api/function_template.py
+++ models_api/function_template.py
@@ -13,7 +13,7 @@ tools = [
                 },
                 "query": {
                     "type": "string",
-                    "description": """a SQL query. Note that the query should be in Google SQL syntax. If the query does not contain all the primary key columns make sure it deduplicates records. The result should always be aggregated and may or may not be grouped by appropriate column(s). Do not group by numeric columns. Avoid zero-division error in calculation. Sort results by suitable columns and use appropriate aliases where applicable."""
+                    "description": """a SQL query. Note that the query should be in Google SQL syntax. Use only the columns names in the schema of the used table. Do not quote the column names. Deduplicate the non-numeric columns in the query to avoid returning too many records. Note that the user requires results in summarized form so aggregate the numeric columns, grouping by appropriate columns. But do not group by numeric columns or calculated expressions. Use appropriate aliases where applicable. Use only the formulae and hints provided as reference for the calculations and strictly follow all the given rules."""
                 }
             },
             "required": [
--- models_api/system_prompt.py
+++ models_api/system_prompt.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 
-system_prompt = """
-You are a business analyst. You have to help the user find an answer to their question. Given a question, use the declared tools/ functions to get the necessary information. Don't assume. Use only the available information.
+now = datetime.today().strftime('%d-%m-%Y %I:%S %p')
+
+system_prompt = f"""
+You are a business analyst. You have to help the user find an answer to their question. Given a question, use the declared tools/ functions to get the necessary information. Don't assume. Use only the available information. Your response should be clean and readable. All numeric values in your response should be rounded to two decimal places. Use appropriate units of measurement for the numeric vlaues in your response.
+You may be required to evaluate time intervals and find dates to answer some questions. Consider the time right now to be {now} as a reference.
 A tool/ function call can return an error if not used correctly. Use the error message to correct your inputs and retry calling the tool/ function. Do not retry more than thrice.
 If there isn't enough information or tools to answer the question you can prompt the user to provide the necessary input. Be precise and only ask for the necessary inputs.
 If nothing works and you cannot arrive at an answer finally let the user know.
