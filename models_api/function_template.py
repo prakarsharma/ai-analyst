910e2fec26c76f204f1f7dbf6ebd2ec567b4b7bb
Prakarsh <prakarsh.sharma@walmart.com>
2024-09-17 12:52:43 +0000

    updated function prompt

--- models_api/function_template.py
+++ models_api/function_template.py
@@ -13,7 +13,7 @@ tools = [
                 },
                 "query": {
                     "type": "string",
-                    "description": """a SQL query. Note that the query should be in Google SQL syntax. Use only the columns names in the schema of the used table. Do not quote the column names. Deduplicate the non-numeric columns in the query to avoid returning too many records. Note that the user requires results in summarized form so aggregate the numeric columns, grouping by appropriate columns. But do not group by numeric columns or calculated expressions. Use appropriate aliases where applicable. Use only the formulae and hints provided as reference for the calculations and strictly follow all the given rules."""
+                    "description": """a SQL query. Note that the query should be in Google SQL syntax. Use only the columns names in the schema of the used table. Do not quote the column names. Deduplicate the non-numeric columns in the query to avoid returning too many records. Note that the user requires results in summarized form so aggregate the numeric columns, grouping by appropriate columns. But do not group by numeric columns or calculated expressions. Include the columns used to group by in the results. Use appropriate aliases where applicable. Use only the formulae and hints provided as reference for the calculations and strictly follow all the given rules."""
                 }
             },
             "required": [
